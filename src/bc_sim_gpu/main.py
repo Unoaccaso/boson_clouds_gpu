ec707d8cd229b72b6805f3d7578bea6b8ceae742
Unoaccaso <felicettriccardo1@gmail.com>
2023-12-07 18:54:33 +0100

    mass and spin generation implemented

--- src/bc_sim_gpu/main.py
+++ src/bc_sim_gpu/main.py
@@ -13,11 +13,32 @@ import numpy
 
 def main():
     # Importing settings from file
-    config = configparser.ConfigParser()
-    config.read("settings.ini")
+    settings = configparser.ConfigParser()
+    settings.read("settings.ini")
 
-    # Computing distances
-    distances = analysis.compute.random_distances(config)
+    # Generate sky positions and compute distances
+    distances = analysis.compute.random_distances(settings)
+
+    # Generating sources
+    N_sources = int(settings["SIMULATION"]["N_BHS_IN_GALACTIC_CENTER"]) + (
+        int(settings["SIMULATION"]["N_BHS_IN_HALO_CLUSTERS"])
+        * int(settings["SIMULATION"]["N_CLUSTERS_IN_HALO"])
+    )
+    BH_masses = generate.physics.Masses(
+        N_sources,
+        float(settings["SIMULATION"]["BH_MASS_MIN"]),
+        float(settings["SIMULATION"]["BH_MASS_MAX"]),
+    ).kroupa()
+    boson_masses = generate.physics.Masses(
+        N_sources,
+        float(settings["SIMULATION"]["BOSON_MASS_MIN"]),
+        float(settings["SIMULATION"]["BOSON_MASS_MAX"]),
+    ).logspace()
+    spins = generate.physics.Spins(
+        N_sources,
+        float(settings["SIMULATION"]["SPIN_MIN"]),
+        float(settings["SIMULATION"]["SPIN_MAX"]),
+    ).truncated_norm()
 
 
 if __name__ == "__main__":
